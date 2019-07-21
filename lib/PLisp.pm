a472705174e9f5a2beffd60417cc958c6c47f251
Richard Levitte <richard@levitte.org>
2019-07-21 22:47:58 +0200

    Add the PLisp module
    This should include everything essential

---
+++ lib/PLisp.pm
@@ -0,0 +1,16 @@
+# Copyright (c) 2019, Richard Levitte
+# All rights reserved.
+#
+# Licensed under the BSD 2-Clause License (the "License").
+# You can obtain a copy in the file LICENSE in the source distribution.
+
+use strict;
+use warnings;
+
+package PLisp;
+
+use PLisp::BuiltinValues;
+use PLisp::BuiltinPackages;
+use PLisp::BuiltinReadtables;
+
+1;
